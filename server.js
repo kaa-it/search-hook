b7b2d8324e8c7492e94b4fd75de16b00b0d46580
Andrey Kruglov <kruglov.it@gmail.com>
2023-05-31 08:43:14 +0300

    Complete version

--- server.js
+++ server.js
@@ -4,12 +4,20 @@ const cities = require('./cities');
 
 app.use(express.json());
 
-app.get('/search', (request, response) => {
+app.get("/search", (request, response) => {
     const terms = request.query.terms.toUpperCase();
-    response.send(cities.filter(c => {
-        return (c.name.toUpperCase().indexOf(terms.toUpperCase()) !== -1)
-            || (c.subject.toUpperCase().indexOf(terms.toUpperCase()) !== -1);
-    }));
+    setTimeout(
+        () =>
+            response.send(
+                cities.filter((c) => {
+                    return (
+                        c.name.toUpperCase().indexOf(terms.toUpperCase()) !== -1 ||
+                        c.subject.toUpperCase().indexOf(terms.toUpperCase()) !== -1
+                    );
+                })
+            ),
+        3000
+    );
 });
 
 app.listen(5000, () => console.log('🚀 Запущен на порту 5000!'));
--- src/app.js
+++ src/app.js
@@ -1,53 +1,90 @@
-import './app.css';
-import {useEffect, useState} from "react";
+import "./app.css";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
-function App() {
-    const [terms, setTerms] = useState('');
+const useSearch = (terms) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
 
-    const [results, setResults] = useState([]);
+  useEffect(() => {
+    setError(null);
+    if (terms) {
+      const source = axios.CancelToken.source();
+      (async () => {
+        try {
+          setLoading(true);
+          const response = await axios.get("/search", {
+            params: { terms },
+            cancelToken: source.token,
+          });
 
-    useEffect(() => {
-        if (terms) {
-            axios.get("/search", {
-                params: {terms},
-            })
-                .then(res => setResults(res.data))
-                .catch(err => console.log(err));
+          setData(response.data);
+          setLoading(false);
+        } catch (err) {
+          if (err.message === "axios request canceled") {
+            return;
+          }
+
+          setError(err);
+          setLoading(false);
         }
-    }, [terms]);
+      })();
+      return () => {
+        source.cancel("axios request canceled");
+      };
+    } else {
+      setData([]);
+      setLoading(false);
+    }
+  }, [terms]);
 
-    return (
-        <div className="app">
-            <input
-                className="search-input"
-                placeholder="Поиск..."
-                type="text"
-                value={terms}
-                onChange={(e) => setTerms(e.target.value)}
-            />
-            {results && results.length ? (
-                <table>
-                    <thead>
-                    <tr>
-                        <th>Город</th>
-                        <th>Субъект</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {results.map((r, index) => (
-                        <tr key={index}>
-                            <td>{r.name}</td>
-                            <td>{r.subject}</td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </table>
-            ) : (
-                <p>Нет результатов</p>
-            )}
-        </div>
-    );
+  return { data, loading, error };
+};
+
+function App() {
+  const [terms, setTerms] = useState("");
+
+  const { data: results, loading, error } = useSearch(terms);
+
+  return (
+      <div className="app">
+        <input
+            className="search-input"
+            placeholder="Поиск..."
+            type="text"
+            value={terms}
+            onChange={(e) => setTerms(e.target.value)}
+        />
+        {error ? (
+            <div className="error">
+              Что-то пошло не так:
+              <div className="error-contents">{error.message}</div>
+            </div>
+        ) : loading ? (
+            <div className="loading">Поиск...</div>
+        ) : results && results.length ? (
+            <table>
+              <thead>
+              <tr>
+                <th>Город</th>
+                <th>Субъект</th>
+              </tr>
+              </thead>
+              <tbody>
+              {results.map((r, index) => (
+                  <tr key={index}>
+                    <td>{r.name}</td>
+                    <td>{r.subject}</td>
+                  </tr>
+              ))}
+              </tbody>
+            </table>
+        ) : (
+            <p>Нет результатов</p>
+        )}
+      </div>
+  );
 }
 
 export default App;
